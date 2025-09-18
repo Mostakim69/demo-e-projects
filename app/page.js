4ed8bb62e11b0c3b75d84e94d945f7b7f7680e6b
newaz2796 <newaz2796@gmail.com>
2025-09-18 23:51:01 +0600

    added testimonial section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import Navbar from "./components/shared/Navbar";
-import Footer from "./components/shared/footer/Footer";
+import Testimonial from "./components/home/Testimonial/Testimonial";
 
 export default function Home() {
   return (
     <main>
-     <Navbar></Navbar>
-     <Footer></Footer>
+      hello world
+      <Testimonial/>
     </main>
   );
 }
