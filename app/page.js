9da68a086da4888e6a70508b96886df3d85893ec
md-asgor-ali <asgor542@gmail.com>
2025-09-18 21:05:44 +0600

    i am created newsletter section and footer

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/footer/Footer";
+import Newsletter from "./components/newsletter/Newsletter";
 
 export default function Home() {
   return (
     <main>
      <Navbar></Navbar>
+     <Newsletter></Newsletter>
      <Footer></Footer>
     </main>
   );
