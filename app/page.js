9fffc798abb6a2e4f96eee3def097fb2c17d14ef
sharmin-khan <sharminkhancse@gmail.com>
2025-09-19 00:34:31 +0600

    added Special Offers section

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/footer/Footer";
 import PopularProduct from "./components/home/PopularProduct";
 import Banner from "./components/home/Banner";
+import SpecialOffers from "./components/home/SpecialOffers";
 
 export default function Home() {
   return (
     <main>
      <Navbar></Navbar>
      <PopularProduct/>
+     <SpecialOffers/>
      <Banner></Banner>
      <Footer></Footer>
     </main>
