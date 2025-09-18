91938d6f2812ca6f228b236dcc3cb44a28248a12
sharmin-khan <sharminkhancse@gmail.com>
2025-09-18 23:26:43 +0600

    Completed Populer product section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/footer/Footer";
+import PopularProduct from "./components/home/PopularProduct";
 
 export default function Home() {
   return (
     <main>
      <Navbar></Navbar>
+     <PopularProduct/>
      <Footer></Footer>
     </main>
   );
