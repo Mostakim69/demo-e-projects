e94b134dc0409bfe2129ded1b79367cc8fa07d89
sharmin-khan <sharminkhancse@gmail.com>
2025-09-19 11:22:32 +0600

    Resolve merge conflict in page.js

--- app/page.js
+++ app/page.js
@@ -4,19 +4,15 @@ import Navbar from "./components/shared/Navbar";
 import Footer from "./components/shared/footer/Footer";
 import Newsletter from "./components/newsletter/Newsletter";
 import PopularProduct from "./components/home/PopularProduct";
-<<<<<<< HEAD
 import Banner from "./components/home/Banner";
 import SpecialOffers from "./components/home/SpecialOffers";
-=======
 import Catagories from "./components/catagories/Catagories";
 import FeaturedProducts from "./components/featuredProducts/FeaturedProducts";
 import Banner from "./components/home/banner/Banner";
->>>>>>> a634b0bb7f0671a79163eceb9ae8f4c2a36cee9b
 
 export default function Home() {
   return (
     <main>
-<<<<<<< HEAD
       hello world
       <Testimonial/>
      <Navbar></Navbar>
@@ -25,7 +21,6 @@ export default function Home() {
      <SpecialOffers/>
      <Banner></Banner>
      <Footer></Footer>
-=======
       <Navbar></Navbar>
       <Banner />
       <Catagories />
@@ -34,7 +29,6 @@ export default function Home() {
       <Testimonial />
       <Newsletter />
       <Footer />
->>>>>>> a634b0bb7f0671a79163eceb9ae8f4c2a36cee9b
     </main>
   );
 }
